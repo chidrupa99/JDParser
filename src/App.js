f7b48f34c9a672965363de01400ff828b2eb1265
Shashank Davalgi Omprakash <sdavalgi@sundevils.asu.edu>
2023-09-10 14:36:50 -0700

    Update Salary and Experience Parser

--- src/App.js
+++ src/App.js
@@ -9,38 +9,40 @@ function App() {
 
   const parseJobDescription = (jobDescription) => {
     // Define regular expressions to capture relevant information
-    const salarySentenceRegex = /[^.!?]*\$\s*[^.!?]*/gi;
-    const experienceSentenceRegex = /[^.!?]*\d+\+\s*[^.!?]*/gi;
+    const salaryRegex = /[^.!?]*(?:\$|USD)[^.!?]*[.!?]/g;
+    const experienceSentenceRegex = /\b(\d+\s?\+\s?years?(?:\s?(?:of)?\s?(?:professional)?\s?(?:work)?\s?(?:experience)?)?)\b/gi;
     const locationRegex = /\b(\b[A-Za-z\s]+,\s?[A-Za-z\s]+\b)/gi;
     const visaSentenceRegex = /.*\bvisa\b.*?[.!?]/i;
   
     // Extract information using regular expressions
-    const salarySentences = jobDescription.match(salarySentenceRegex);
+    const salaryMatches = jobDescription.match(salaryRegex);
     const experienceSentences = jobDescription.match(experienceSentenceRegex);
     const locationMatches = jobDescription.match(locationRegex);
     const visaSentenceMatch = jobDescription.match(visaSentenceRegex);
   
     // Extracted information arrays
-    // const extractedSalaries = salaryMatches || [];
+    const extractedSalaries = salaryMatches || [];
+    const formattedSalaries = extractedSalaries.map((sentence, index) => (
+      <li key={index}>{sentence.trim()}</li>
+    ));
     // const extractedExperiences = experienceMatches || [];
     const extractedLocations = locationMatches || [];
     // const extractedVisaInfo = visaMatches || [];
   
     // Convert arrays to strings
-    const salaryInfo = salarySentences ? salarySentences.join('\n') : 'Salary information not found';
-    // const experience = extractedExperiences.join(', ') || 'Not specified';
     let experienceInfo = 'Experience information not found';
-  if (experienceSentences) {
-    experienceInfo = (
-      <ul>
-        {experienceSentences.map((sentence, index) => (
-          <li key={index}>{sentence}</li>
-        ))}
-      </ul>
-    );
-  }
+    if (experienceSentences) {
+      experienceInfo = (
+        <ul>
+          {experienceSentences.map((sentence, index) => (
+            <li key={index}>{sentence}</li>
+          ))}
+        </ul>
+      );
+    }
+
     const location = extractedLocations.join(', ') || 'Not specified';
-    // const visaInfo = extractedVisaInfo.join(', ') || 'Not specified';
+    
     let visaInfo = 'Visa information not found';
     if (visaSentenceMatch && visaSentenceMatch.length > 0) {
       visaInfo = visaSentenceMatch[0].trim();
@@ -80,7 +82,7 @@ function App() {
     // const visaInfo = "Bachelor's Degree in Industrial Design, Product Design, Human Computer Interaction, or related field AND 3+ years of industry experience working in product or service design, especially on complex problems, OR equivalent experience (e.g., experience using design thinking to solve problems)."
 
     setParsedData({
-      salary: salaryInfo,
+      salary: formattedSalaries.length > 0 ? formattedSalaries : ['Not specified'],
       experience: experienceInfo,
       skills,
       visaInfo: visaInfo,
