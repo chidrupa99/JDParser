ed6ca81a787b03da2c9d4b1ef1f8ad9cb4ca4c11
Shashank Davalgi Omprakash <sdavalgi@sundevils.asu.edu>
2023-09-10 12:47:42 -0700

    Salary Parser

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ function App() {
 
   const parseJobDescription = (jobDescription) => {
     // Define regular expressions to capture relevant information
-    const salaryRegex = /\b(\$?[0-9,]+(?:\.[0-9]+)?\s*(?:K|k|thousand|million|billion)?\s*(?:USD|US dollars|dollars|USD annually|annually|per annum|\/year)?\b)/gi;
+    const salarySentenceRegex = /[^.!?]*\$\s*[^.!?]*/gi;
     const experienceRegex = /\b(\d+(?:-\d+)?\s?\+?\s?years?(?:\s?(?:of)?\s?(?:professional)?\s?(?:work)?\s?(?:experience)?)?)\b/gi;
     const locationRegex = /\b(\b[A-Za-z\s]+,\s?[A-Za-z\s]+\b)/gi;
     const visaRegex = /\b(?:visa|work visa|H1B|H-1B|J-1|OPT|CPT|TN visa|sponsorship)\b/gi;
   
     // Extract information using regular expressions
-    const salaryMatches = jobDescription.match(salaryRegex);
+    const salarySentences = jobDescription.match(salarySentenceRegex);
     const experienceMatches = jobDescription.match(experienceRegex);
     const locationMatches = jobDescription.match(locationRegex);
     const visaMatches = jobDescription.match(visaRegex);
   
     // Extracted information arrays
-    const extractedSalaries = salaryMatches || [];
+    // const extractedSalaries = salaryMatches || [];
     const extractedExperiences = experienceMatches || [];
     const extractedLocations = locationMatches || [];
     const extractedVisaInfo = visaMatches || [];
   
     // Convert arrays to strings
-    const salary = extractedSalaries.join(', ') || 'Not specified';
+    const salaryInfo = salarySentences ? salarySentences.join('\n') : 'Salary information not found';
     const experience = extractedExperiences.join(', ') || 'Not specified';
     const location = extractedLocations.join(', ') || 'Not specified';
     const visaInfo = extractedVisaInfo.join(', ') || 'Not specified';
   
     // Update the state with the extracted information
     setParsedData({
-      salary,
+      salary: salaryInfo,
       experience,
       location,
       visaInfo,
